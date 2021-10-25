93e58dfe8dc3bab474a9283c5f746745f825e418
Simon Koch <simon.koch@tu-braunschweig.de>
2021-10-25 12:53:52 +0200

    added color wheel to work with multiple different colors - nyi of custom color generation in case colors are used up

---
+++ src/main/scala/de/tubs/ias/ppm/tikzGeneral/ColorWheel.scala
@@ -0,0 +1,43 @@
+package de.tubs.ias.ppm.tikzGeneral
+
+import collection.mutable.{Map => MMap}
+
+object NoMoreColorException extends Throwable {
+  override def getMessage: String = "there are no more available colors in the color wheel"
+}
+
+class ColorWheel(amount : Int = 18) {
+
+  private val used : MMap[Color,Boolean] = MMap(
+    Red -> false,
+    Blue -> false,
+    Yellow -> false,
+    Green -> false,
+    Cyan -> false,
+    Magenta -> false,
+    Black -> false,
+    Grey -> false,
+    Darkgray -> false,
+    Lightgray -> false,
+    Brown -> false,
+    Lime -> false,
+    Olive -> false,
+    Orange -> false,
+    Pink -> false,
+    Purple  -> false,
+    Teal  -> false,
+    Violet -> false
+  )
+
+
+  def getNextColor : Color = {
+    val hit: Option[(Color, Boolean)] = used.find(!_._2)
+    hit match {
+      case Some((color,_)) =>
+        used(color) = true
+        color
+      case None => throw NoMoreColorException
+    }
+  }
+
+}
