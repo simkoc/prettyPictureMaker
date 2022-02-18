e75cfdb7291cc3214c5330d9a250c95d06239b32
Simon Koch <simon.koch@tu-braunschweig.de>
2022-02-18 17:05:06 +0100

    added enum for different font sizes

---
+++ src/main/scala/de/halcony/ppm/graph/generics/FontSizes.scala
@@ -0,0 +1,9 @@
+package de.halcony.ppm.graph.generics
+
+object FontSizes extends Enumeration {
+
+  type FontSize = Value
+  val FOOTNOTESIZE: FontSizes.Value = Value("\\footnotesize")
+  val TINY: FontSizes.Value = Value("\\tiny")
+
+}
