f85aa294c4b7521eb572288f4b4f0757c1e5766a
Simon Koch <simon.koch@tu-braunschweig.de>
2022-02-18 17:04:04 +0100

    added further configuration options for any axis

--- src/main/scala/de/halcony/ppm/graph/generics/Axis.scala
+++ src/main/scala/de/halcony/ppm/graph/generics/Axis.scala
@@ -1,8 +1,9 @@
 package de.halcony.ppm.graph.generics
 import de.halcony.ppm.colors.CustomColor
-import de.halcony.ppm.graph.generics.Side._
 import de.halcony.ppm.graph.legends.Legend
+import de.halcony.ppm.utility.OptionExtensions.ExtendedOption
 import wvlet.log.LogSupport
+
 import scala.collection.mutable.ListBuffer
 
 trait Axis extends Plottable with LogSupport {
@@ -15,7 +16,60 @@ trait Axis extends Plottable with LogSupport {
   private var ymin: Option[Int] = None
   private var ymax: Option[Int] = None
   private var legend: Option[Legend] = None
-  private var side: Side = LEFT
+  private var xAxisAlignment: Option[AxisAlignment.Alignment] = None
+  private var yAxisAlignment: Option[AxisAlignment.Alignment] = None
+  private var xtickspt: Option[Int] = None
+  private var ytickspt: Option[Int] = None
+  private var yticklabels: Boolean = true
+  private var xticklabels: Boolean = true
+  private var enlargeLimits: Option[Double] = None
+  private var drawXTicks: Boolean = true
+  private var drawYTicks: Boolean = true
+
+  def disableXTicks(): Axis = {
+    drawXTicks = false
+    this
+  }
+
+  def disableYTicks(): Axis = {
+    drawYTicks = false
+    this
+  }
+
+  def enlargeLimits(factor: Double): Axis = {
+    enlargeLimits = Some(factor)
+    this
+  }
+
+  def setNoYTickLabels(): Axis = {
+    yticklabels = false
+    this
+  }
+
+  def setNoXTickLabels(): Axis = {
+    xticklabels = false
+    this
+  }
+
+  def setXTicksSpacing(pt: Int): Axis = {
+    xtickspt = Some(pt)
+    this
+  }
+
+  def setYTicksSpacing(pt: Int): Axis = {
+    ytickspt = Some(pt)
+    this
+  }
+
+  def yAxisAlignment(alignment: AxisAlignment.Alignment): Axis = {
+    yAxisAlignment = Some(alignment)
+    this
+  }
+
+  def xAxisAlignment(alignment: AxisAlignment.Alignment): Axis = {
+    xAxisAlignment = Some(alignment)
+    this
+  }
 
   def addPlot(plot: Plot): Axis = {
     plots.addOne(plot)
@@ -39,11 +93,6 @@ trait Axis extends Plottable with LogSupport {
 
   def setXMin(xmin: Int): Axis = {
     this.xmin = Some(xmin)
-    this
-  }
-
-  def setSide(side: Side): Axis = {
-    this.side = side
     this
   }
 
@@ -73,8 +122,8 @@ trait Axis extends Plottable with LogSupport {
   override def plot: String = {
     val plot =
       s"""\\begin{axis}[
-        $getBoilerplateCode
         $customAxisConfigurationLines
+        $getBoilerplateCode
        ]
        ${plots.map(_.plot).mkString("\n")}
        ${if (legend.nonEmpty) legend.get.plot(plots.toList) else ""}
@@ -86,12 +135,22 @@ trait Axis extends Plottable with LogSupport {
 
   private def getBoilerplateCode: String = {
     s"""${if (width.nonEmpty) s"width=${width.get}cm," else ""}
-        axis y line*=${if (side == LEFT) "left" else "right"},
         ${if (height.nonEmpty) s"height=${height.get}cm," else ""}
+        ${yAxisAlignment.processOrElse(value => s"axis y line=$value,", "")}
+        ${xAxisAlignment.processOrElse(value => s"axis x line=$value,", "")}
+        ${ytickspt.processOrElse(value => s"y=${value}pt,", "")}
+        ${xtickspt.processOrElse(value => s"x=${value}pt,", "")}
+        ${enlargeLimits.processOrElse(
+      value => s"enlargelimits={lower, $value},",
+      "")}
         ${if (xmin.nonEmpty) s"xmin=${xmin.get}," else ""}
         ${if (xmax.nonEmpty) s"xmax=${xmax.get}," else ""}
         ${if (ymin.nonEmpty) s"ymin=${ymin.get}," else ""}
-        ${if (ymax.nonEmpty) s"ymax=${ymax.get}," else ""}"""
+        ${if (ymax.nonEmpty) s"ymax=${ymax.get}," else ""}
+        ${if (!drawXTicks) s"xtick style={draw=none}," else ""}
+        ${if (!drawYTicks) s"ytick style={draw=none}," else ""}
+        ${if (!xticklabels) "xticklabels=\\empty," else ""}
+        ${if (!yticklabels) "yticklabels=\\empty," else ""}"""
   }
 
 }
